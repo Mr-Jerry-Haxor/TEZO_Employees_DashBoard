99f44b48e97febe90d115adf4f51dd853b8dfac4
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2024-02-07 17:29:54 +0530

    formatted

--- index.js
+++ index.js
@@ -1,90 +1,88 @@
 function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    const mainContainer = document.querySelector('.main-container');
-    sidebar.classList.toggle('toggleopen');
-    mainContainer.classList.toggle('sidebar-open');
-
+  const sidebar = document.querySelector(".sidebar");
+  const mainContainer = document.querySelector(".main-container");
+  sidebar.classList.toggle("toggleopen");
+  mainContainer.classList.toggle("sidebar-open");
 }
 // including sidebar html code
 
-const sidebarcontainer = document.querySelector('.sidebar-container');
-fetch('https://raw.githubusercontent.com/Mr-Jerry-Haxor/TEZO_Employees_DashBoard/main/sidebar.html')
-.then(res=>res.text())
-.then(data=>{
+const sidebarcontainer = document.querySelector(".sidebar-container");
+fetch(
+  "https://raw.githubusercontent.com/Mr-Jerry-Haxor/TEZO_Employees_DashBoard/main/sidebar.html"
+)
+  .then((res) => res.text())
+  .then((data) => {
     sidebarcontainer.innerHTML = data;
-});
-
+  });
 
 // including searchbar html code
 
-const searchbar = document.querySelector('.searchbar-container');
-fetch('https://raw.githubusercontent.com/Mr-Jerry-Haxor/TEZO_Employees_DashBoard/main/searchbar.html')
-.then(res=>res.text())
-.then(data=>{
+const searchbar = document.querySelector(".searchbar-container");
+fetch(
+  "https://raw.githubusercontent.com/Mr-Jerry-Haxor/TEZO_Employees_DashBoard/main/searchbar.html"
+)
+  .then((res) => res.text())
+  .then((data) => {
     searchbar.innerHTML = data;
-});
-
-
-
-
+  });
 
 // onclick of emeployee in the sidebar, load the employee.html filea and display it in the main content area
 
 function EmployeeMenu() {
-    const mainContainer = document.querySelector('.main-content');
-    fetch('https://raw.githubusercontent.com/Mr-Jerry-Haxor/TEZO_Employees_DashBoard/main/employee.html')
-    .then(res=>res.text())
-    .then(data=>{
-        mainContainer.innerHTML = data;
+  const mainContainer = document.querySelector(".main-content");
+  fetch(
+    "https://raw.githubusercontent.com/Mr-Jerry-Haxor/TEZO_Employees_DashBoard/main/employee.html"
+  )
+    .then((res) => res.text())
+    .then((data) => {
+      mainContainer.innerHTML = data;
     });
-    const employeeMenu = document.querySelector('#employee-menu');
-    employeeMenu.classList.add('menuactive');
-    // remove active class from other menu
-    const rolesMenu = document.querySelector('#roles-menu');
-    rolesMenu.classList.remove('menuactive');
+  const employeeMenu = document.querySelector("#employee-menu");
+  employeeMenu.classList.add("menuactive");
+  // remove active class from other menu
+  const rolesMenu = document.querySelector("#roles-menu");
+  rolesMenu.classList.remove("menuactive");
 }
 
 function RolesMenu() {
-    const mainContainer = document.querySelector('.main-content');
-    fetch('https://raw.githubusercontent.com/Mr-Jerry-Haxor/TEZO_Employees_DashBoard/main/roles.html')
-    .then(res=>res.text())
-    .then(data=>{
-        mainContainer.innerHTML = data;
+  const mainContainer = document.querySelector(".main-content");
+  fetch(
+    "https://raw.githubusercontent.com/Mr-Jerry-Haxor/TEZO_Employees_DashBoard/main/roles.html"
+  )
+    .then((res) => res.text())
+    .then((data) => {
+      mainContainer.innerHTML = data;
     });
-    const rolesMenu = document.querySelector('#roles-menu');
-    rolesMenu.classList.add('menuactive');
-    // remove active class from other menu
-    const employeeMenu = document.querySelector('#employee-menu');
-    employeeMenu.classList.remove('menuactive');
+  const rolesMenu = document.querySelector("#roles-menu");
+  rolesMenu.classList.add("menuactive");
+  // remove active class from other menu
+  const employeeMenu = document.querySelector("#employee-menu");
+  employeeMenu.classList.remove("menuactive");
 }
-
 
 // role details page
 function RoleDetails() {
-    const mainContainer = document.querySelector('.main-content');
-    fetch('/roledetails.html')
-  .then(res=>res.text())
-    .then(data=>{
-        mainContainer.innerHTML = data;
+  const mainContainer = document.querySelector(".main-content");
+  fetch("/roledetails.html")
+    .then((res) => res.text())
+    .then((data) => {
+      mainContainer.innerHTML = data;
     });
-    const employeeMenu = document.querySelector('#employee-menu');
-    employeeMenu.classList.remove('menuactive');
-    // remove active class from other menu
-    const rolesMenu = document.querySelector('#roles-menu');
-    rolesMenu.classList.remove('menuactive');
+  const employeeMenu = document.querySelector("#employee-menu");
+  employeeMenu.classList.remove("menuactive");
+  // remove active class from other menu
+  const rolesMenu = document.querySelector("#roles-menu");
+  rolesMenu.classList.remove("menuactive");
 }
-
 
 // default  employee page load
 function defaultpage() {
-    const mainContainer = document.querySelector('.main-content');
-    fetch('https://raw.githubusercontent.com/Mr-Jerry-Haxor/TEZO_Employees_DashBoard/main/employee.html')
-    .then(res=>res.text())
-    .then(data=>{
-        mainContainer.innerHTML = data;
+  const mainContainer = document.querySelector(".main-content");
+  fetch("/roledetails.html")
+    .then((res) => res.text())
+    .then((data) => {
+      mainContainer.innerHTML = data;
     });
-    const employeeMenu = document.querySelector('#employee-menu');
-    employeeMenu.classList.toggle('menuactive');
+  const employeeMenu = document.querySelector("#employee-menu");
+  employeeMenu.classList.toggle("menuactive");
 }
-
-
