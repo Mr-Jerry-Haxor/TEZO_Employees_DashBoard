7d2896ddea21cd4853124247af83df208f906f64
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2024-02-07 13:37:26 +0530

    dynamic page load

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
-function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    const mainContainer = document.querySelector('.main-container');
-    sidebar.classList.toggle('toggleopen');
-    mainContainer.classList.toggle('sidebar-open');
+
+// including sidebar html code
+
+const sidebar = document.querySelector('.sidebar-container');
+fetch('/sidebar.html')
+.then(res=>res.text())
+.then(data=>{
+    sidebar.innerHTML = data;
+});
+
+
+// including searchbar html code
+
+const searchbar = document.querySelector('.searchbar-container');
+fetch('/searchbar.html')
+.then(res=>res.text())
+.then(data=>{
+    searchbar.innerHTML = data;
+});
+
+
+
+
+
+// onclick of emeployee in the sidebar, load the employee.html filea and display it in the main content area
+
+function EmployeeMenu() {
+    const mainContainer = document.querySelector('.main-content');
+    fetch('/employee.html')
+    .then(res=>res.text())
+    .then(data=>{
+        mainContainer.innerHTML = data;
+    });
+    const employeeMenu = document.querySelector('#employee-menu');
+    employeeMenu.classList.add('menuactive');
+    // remove active class from other menu
+    const rolesMenu = document.querySelector('#roles-menu');
+    rolesMenu.classList.remove('menuactive');
 }
 
-function toggleSubmenu(submenuId) {
-    const submenu = document.querySelector(`#${submenuId}`);
-    submenu.classList.toggle('submenuopen');
+function RolesMenu() {
+    const mainContainer = document.querySelector('.main-content');
+    fetch('/roles.html')
+    .then(res=>res.text())
+    .then(data=>{
+        mainContainer.innerHTML = data;
+    });
+    const rolesMenu = document.querySelector('#roles-menu');
+    rolesMenu.classList.add('menuactive');
+    // remove active class from other menu
+    const employeeMenu = document.querySelector('#employee-menu');
+    employeeMenu.classList.remove('menuactive');
 }
+
+// default  employee page load
+function defaultpage() {
+    const mainContainer = document.querySelector('.main-content');
+    fetch('/employee.html')
+    .then(res=>res.text())
+    .then(data=>{
+        mainContainer.innerHTML = data;
+    });
+    const employeeMenu = document.querySelector('#employee-menu');
+    employeeMenu.classList.toggle('menuactive');
+}
+
+
