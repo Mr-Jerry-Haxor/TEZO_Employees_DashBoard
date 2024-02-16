adade64bff0cb4b0e1c152bf082f9b1746d26f14
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2024-02-16 15:00:48 +0530

    Update addemployee.js

--- JavaScript/addemployee.js
+++ JavaScript/addemployee.js
@@ -23,6 +23,31 @@ function addEmployeeFormValidation() {
 }
 
 
+function addemployeeFormSubmitValidation(){
+    document.getElementById('add-employee-form').addEventListener('submit', function(event) {
+        event.preventDefault(); // prevent form submission
+        var fields = ['empid', 'firstname', 'lastname', 'dob', 'email', 'mobile' , 'joiningdate', 'location', 'department'];
+        // Loop through each field
+        fields.forEach(function(fieldId) {
+            var field = document.getElementById(fieldId);
+            var errorSpan = document.getElementById(fieldId + '-span');
+
+            // Add event listener to each field
+            field.addEventListener('input', function() {
+                if (!field.checkValidity()) {
+                    field.classList.add('err');
+                    errorSpan.innerHTML = "<img src='assets/exclamation-mark-diamond.svg' alt='error' style='height:15px' > " + "This " + fieldId + ' field is required'
+                    errorSpan.insertText = fieldId + ' is required';
+                } else {
+                    field.classList.remove('err');
+                    errorSpan.innerText = '';
+                }
+            });
+        });
+    });
+}
+
+
 
 function AddEmployeeAlert(status){
     var alertContainer = document.querySelector('.alert-messages');
